7d69baece068505f21617ed55740fb55e1c3ffd7
the3x0rcist <babakm5555@gmail.com>
2024-12-13 14:30:57 +0000

    testing

---
+++ portscanner.py
@@ -0,0 +1,25 @@
+#!/bin/env python3
+
+#my simple portscanner project
+
+import sys
+import socket
+
+
+
+if len(sys.argv) != 3:
+    print("Usage: ./portscanner <ip> <port>")
+    sys.exit(1)
+
+ip = sys.argv[1]
+
+for port in range(1, int(sys.argv[2])):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((ip,port))
+    except:
+        print("[OPEN] port: ", port)
+    s.close()
+
+
+
